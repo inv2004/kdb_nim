b0160124c2d0ca10be4288c757899cc0b5186a18
ae <inv2004@gmail.com>
2020-05-22 21:49:21 +0300

    remove K internal field visibility

--- src/kdb/types.nim
+++ src/kdb/types.nim
@@ -68,98 +68,98 @@ type
     a*: cchar
     case kind*: KKind
     of kList:
-      lu*: cchar
-      lr*: cint
+      lu: cchar
+      lr: cint
       kLen*: int64
       kArr*: UncheckedArray[K0]
     of kVecBool:
-      vbu*: cchar
-      vbr*: cint
+      vbu: cchar
+      vbr: cint
       boolLen*: int64
       boolArr*: UncheckedArray[bool]
     of kVecGUID:
-      vgu*: cchar
-      vgr*: cint
+      vgu: cchar
+      vgr: cint
       guidLen*: int64
       guidArr*: UncheckedArray[GUID]
     of kVecByte:
-      vyu*: cchar
-      vyr*: cint
+      vyu: cchar
+      vyr: cint
       byteLen*: int64
       byteArr*: UncheckedArray[byte]
     of kVecShort:
-      vhu*: cchar
-      vhr*: cint
+      vhu: cchar
+      vhr: cint
       shortLen*: int64
       shortArr*: UncheckedArray[int16]
     of kVecInt:
-      viu*: cchar
-      vir*: cint
+      viu: cchar
+      vir: cint
       intLen*: int64
       intArr*: UncheckedArray[int32]
     of kVecLong:
-      vju*: cchar
-      vjr*: cint
+      vju: cchar
+      vjr: cint
       longLen*: int64
       longArr*: UncheckedArray[int64]
     of kVecReal:
-      vru*: cchar
-      vrr*: cint
+      vru: cchar
+      vrr: cint
       realLen*: int64
       realArr*: UncheckedArray[float32]
     of kVecFloat:
-      vfu*: cchar
-      vfr*: cint
+      vfu: cchar
+      vfr: cint
       floatLen*: int64
       floatArr*: UncheckedArray[float64]
     of kVecChar:
-      vcu*: cchar
-      vcr*: cint
+      vcu: cchar
+      vcr: cint
       charLen*: int64
       charArr*: UncheckedArray[char]
     of kVecSym:
-      vsu*: cchar
-      vsr*: cint
+      vsu: cchar
+      vsr: cint
       stringLen*: int64
       stringArr*: UncheckedArray[cstring]
     of kVecTimestamp:
-      vtsu*: cchar
-      vtsr*: cint
+      vtsu: cchar
+      vtsr: cint
       tsLen*: int64
       tsArr*: UncheckedArray[int64]
     of kVecMonth:
-      vmu*: cchar
-      vmr*: cint
+      vmu: cchar
+      vmr: cint
       monthLen*: int64
       monthArr*: UncheckedArray[int32]
     of kVecDate:
-      vdu*: cchar
-      vdr*: cint
+      vdu: cchar
+      vdr: cint
       dateLen*: int64
       dateArr*: UncheckedArray[int32]
     of kVecDateTime:
-      vdtu*: cchar
-      vdtr*: cint
+      vdtu: cchar
+      vdtr: cint
       dtLen*: int64
       dtArr*: UncheckedArray[float64]
     of kVecTimespan:
-      vtpu*: cchar
-      vtpr*: cint
+      vtpu: cchar
+      vtpr: cint
       tpLen*: int64
       tpArr*: UncheckedArray[int64]
     of kVecMinute:
-      vmiu*: cchar
-      vmir*: cint
+      vmiu: cchar
+      vmir: cint
       minuteLen*: int64
       minuteArr*: UncheckedArray[int32]
     of kVecSecond:
-      vseu*: cchar
-      vser*: cint
+      vseu: cchar
+      vser: cint
       secondLen*: int64
       secondArr*: UncheckedArray[int32]
     of kVecTime:
-      vttu*: cchar
-      vttr*: cint
+      vttu: cchar
+      vttr: cint
       timeLen*: int64
       timeArr*: UncheckedArray[int32]
     of kTable:
@@ -167,91 +167,91 @@ type
       tr*: cint
       dict*: K0
     of kDict:
-      du*: cchar
-      dr*: cint
+      du: cchar
+      dr: cint
       dn*: int64  # always 2
       keys*: K0
       values*: K0
     of kId:
-      idu*: cchar
-      idr*: cint
+      idu: cchar
+      idr: cint
       idg*: byte
     of kError:
-      eru*: cchar
-      err*: cint
+      eru: cchar
+      err: cint
       msg*: cstring
     of kTime:
-      ttu*: cchar
-      ttr*: cint
+      ttu: cchar
+      ttr: cint
       tt*: int32
     of kSecond:
-      seu*: cchar
-      ser*: cint
+      seu: cchar
+      ser: cint
       se*: int32
     of kMinute:
-      miu*: cchar
-      mir*: cint
+      miu: cchar
+      mir: cint
       mi*: int32
     of kTimespan:
-      tpu*: cchar
-      tpr*: cint
+      tpu: cchar
+      tpr: cint
       tp*: int64
     of kDateTime:
-      dtu*: cchar
-      dtr*: cint
+      dtu: cchar
+      dtr: cint
       dt*: float64
     of kDate:
-      eu*: cchar
-      er*: cint
+      eu: cchar
+      er: cint
       dd*: int32
     of kMonth:
-      mu*: cchar
-      mr*: cint
+      mu: cchar
+      mr: cint
       mo*: int32
     of kTimestamp:
-      tsu*: cchar
-      tsr*: cint
+      tsu: cchar
+      tsr: cint
       ts*: int64
     of kSym:
-      su*: cchar
-      sr*: cint
+      su: cchar
+      sr: cint
       ss*: cstring
     of kChar:
-      cu*: cchar
-      cr*: cint
+      cu: cchar
+      cr: cint
       ch*: char
     of kFloat:
-      fu*: cchar
-      fr*: cint
+      fu: cchar
+      fr: cint
       ff*: float64
     of kReal:
-      rru*: cchar
-      rrr*: cint
+      rru: cchar
+      rrr: cint
       rr*: float32
     of kLong:
-      ju*: cchar
-      jr*: cint
+      ju: cchar
+      jr: cint
       jj*: int64
     of kInt:
-      iu*: cchar
-      ir*: cint
+      iu: cchar
+      ir: cint
       ii*: int32
     of kShort:
-      hu*: cchar
-      hr*: cint
+      hu: cchar
+      hr: cint
       sh*: int16
     of kByte:
-      yu*: cchar
-      yr*: cint
+      yu: cchar
+      yr: cint
       by*: byte
     of kGUID:
-      gu*: cchar
-      gr*: cint
+      gu: cchar
+      gr: cint
       gn*: clonglong # probably 2
       gg*: GUID
     of kBool:
-      bu*: cchar
-      br*: cint
+      bu: cchar
+      br: cint
       bb*: bool
 
   K* = object
