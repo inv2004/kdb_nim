13ebe2070b70e579375f44e94b1b0e169357e17d
ae <inv2004@gmail.com>
2020-05-23 03:23:00 +0300

    less iterators code

--- src/kdb/iters.nim
+++ src/kdb/iters.nim
@@ -1,12 +1,27 @@
 
-proc get*[T](x: K0, i: int): T =
+proc get*[T](x: K, i: int): T =
+  when T is K:
+    case x.kind
+    of kList: K(k: x.k.kArr[i])
+    of kVecInt: x.k.intArr[i].toK()
+    of kVecLong: x.k.longArr[i].toK()
+    of kVecSym: x.k.stringArr[i].toSym()
+    else: raise newException(KError, "get[K] is not supported for " & $x.kind)
+  elif T is int64: x.k.longArr[i]
+  elif T is float64: x.k.floatArr[i]
+  elif T is cstring: x.k.stringArr[i]
+  elif T is string: $x.k.stringArr[i]
+  else: raise newException(KError, "get is not supported for " & $x.kind)
+
+proc getM*[T](x: K0, i: int): var T =
   when T is K: x.kArr[i]
   elif T is int64: x.longArr[i]
   elif T is float64: x.floatArr[i]
   elif T is cstring: x.stringArr[i]
   elif T is string: $x.stringArr[i]
-  else: raise newException(KError, "`[]` is not supported for " & $x.kind)
+  else: raise newException(KError, "getM is not supported for " & $x.kind)
 
+# TODO: remove after format leaking check
 iterator items*(x: K0): K0 =
   case x.kind
   of kList:
@@ -17,7 +32,7 @@ iterator items*(x: K0): K0 =
   of kVecBool:
     var i = 0
     while i < x.boolLen:
-      yield x.intArr[i].toK().k
+      yield x.boolArr[i].toK().k
       inc(i)
   of kVecInt:
     var i = 0
@@ -49,57 +64,22 @@ iterator items*(x: K): K =
       let v = r1(x.k.kArr[i])  #TODO: not sure
       yield v
       inc(i)
-  of kVecBool:
-    var i = 0
-    while i < x.k.boolLen:
-      yield x.k.boolArr[i].toK()
-      inc(i)
-  of kVecInt:
-    var i = 0
-    while i < x.k.len:
-      yield x.k.get[:int64](i).toK()
-      inc(i)
-  of kVecLong:
-    var i = 0
-    while i < x.k.longLen:
-      yield x.k.longArr[i].toK()
-      inc(i)
-  of kVecSym:
-    var i = 0
-    while i < x.k.stringLen:
-      yield x.k.stringArr[i].toSym()
-      inc(i)
   of kTable:
     var i = 0
     while i < x.k.dict.keys.len:
       yield x.k.dict.keys.stringArr[i].toSym()
       inc(i)
-  else: raise newException(KError, "items is not supported for " & $x.k.kind)
+  else:
+    for i in 0..<x.len:
+      yield x.get[:K](i)
 
 iterator items*[T](x: K, _: typedesc[T]): T =
   for i in 0..<x.len:
     yield x.k.get[:T](i)
 
 iterator mitems*[T](x: var K): var T =
-  when T is int64:
-    assert x.k.kind == KKind.kVecLong
-    var i = 0
-    while i < x.k.longLen:
-      yield x.k.longArr[i]
-      inc(i)
-  elif T is float64:
-    assert x.k.kind == KKind.kVecFloat
-    var i = 0
-    while i < x.k.floatLen:
-      yield x.k.floatArr[i]
-      inc(i)
-  elif T is cstring:
-    assert x.k.kind == KKind.kVecSym
-    var i = 0
-    while i < x.k.stringLen:
-      yield x.k.stringArr[i]
-      inc(i)
-  else: raise newException(KError, "mitems is not supported for " & $x.k.kind)
+  for i in 0..<x.len:
+    yield x.k.getM[:T](i)
 
 iterator pairs*(x: K): (K, K) =
   case x.k.kind
@@ -119,23 +99,3 @@ iterator pairs*[T](x: K, _: typedesc[T]): (int, T) =
   for v in x.items(T):
     yield (i, v)
     inc(i)
-
-# iterator mitems*(x: K0): var K0 =
-  # case x.kind
-  # of kList:
-    # var i = 0
-    # while i < x.kLen:
-      # yield x.kArr[i]
-      # inc(i)
-  # of kVecInt:
-  #   var i = 0
-  #   while i < x.intLen:
-  #     yield x.intArr[i]
-  #     inc(i)
-  # of kVecSym:
-    # var i = 0
-    # while i < x.stringLen:
-      # yield x.stringArr[i]
-      # inc(i)
-  # else: raise newException(KError, "mitems is not supported for " & $x.kind)
-
