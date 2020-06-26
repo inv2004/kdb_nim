89a3325c8f4ed6b58338c13c9f53890f31efee8f
inv2004@gmail.com <inv2004@gmail.com>
2020-06-26 03:50:34 +0300

    example change

--- examples/enrich_mavg.nim
+++ examples/enrich_mavg.nim
@@ -20,21 +20,11 @@ let client = connect("your-server", 9999)
 var sum = 0.0
 
 while true:
-  let (cmd, data) = client.read(RequestT)
-  echo cmd
-  echo data
-  var r = data
-  var resp = r.transform(ReplyT, toSeq(r.price))
+  var (cmd, data) = client.read(RequestT)               # var - because transform wants mutable
+  var resp = data.transform(ReplyT, toSeq(data.price))  
   var mAvg = resp.movingSum
   for x in mAvg.mitems():
     sum += x
     x = sum
   echo resp
   client.reply(resp)
-
-# check isCall(d)
-# check d[0] == %"test"
-# var t = d[1]
-# for x in t.mitems[:int64]:
-#     x *= 2
-# client.sendSyncReply(t)
