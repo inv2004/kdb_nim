cdbd7e3a9edfe7da9260d5abc27661dc84de2dc1
ae <inv2004@gmail.com>
2020-06-27 14:22:17 +0300

    example changed

--- examples/enrich_mavg.nim
+++ examples/enrich_mavg.nim
@@ -19,14 +19,5 @@ let d = {1: s"one", 2: s"two", 3: s"three"}.toTable
 
 while true:
   let (cmd, data) = client.read(RequestT)
-  for x in data.n:
-    echo x
-    echo d.getOrDefault(x)
-  # let resp = data.transform(ReplyT, data.n.mapIt(d.getOrDefault(it)))
-  # let resp = data.transform(ReplyT, v)
-  # var mAvg = resp.movingSum
-  # for x in mAvg.mitems():
-    # sum += x
-    # x = sum
-  # echo resp
-  client.reply(data)
+  let resp = data.transform(ReplyT, data.n.mapIt(d.getOrDefault(it, s"")))
+  client.reply(resp)
